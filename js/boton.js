f85679e9012fda5b227c96149b957f92fdfa4459
AndersonPonced <89653471+AndersonPonced@users.noreply.github.com>
2021-11-07 20:54:47 -0300

    Add files via upload

---
+++ js/boton.js
@@ -0,0 +1,31 @@
+function aviso (){
+
+ alert ("flow")
+};
+
+function cartelito() {
+  alert ("otro flow")
+};
+
+function fondo1() {
+  document.body.style.backgroundColor = "black";
+}
+
+function fondo2() {
+  document.body.style.backgroundColor = "white";
+
+}
+
+function si() {
+document.getElementById("Anderson").innerHTML = "flow";
+}
+
+function AmpliarImg(x) {
+  x.style.height = "20%";
+    x.style.width = "20%";
+}
+
+function NormalImg(x) {
+  x.style.height = "5%";
+    x.style.width = "5%";
+}
